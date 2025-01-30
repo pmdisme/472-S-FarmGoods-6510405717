101ad07388753b962cd882158f214b9019e60938
KAwfang <jitlada.yt@gmail.com>
2025-01-31 00:02:57 +0700

    modify attributes in product and order detail

---
+++ prisma/migrations/20250130165754_add_qty_in_order_deatail_del_qty_in_product/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `productQuantity` on the `Product` table. All the data in the column will be lost.
+  - Added the required column `quantity` to the `OrderDetail` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "OrderDetail" ADD COLUMN     "quantity" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "productQuantity";
